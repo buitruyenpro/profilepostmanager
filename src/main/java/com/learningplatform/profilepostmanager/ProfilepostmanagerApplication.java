fc45e61005427c9aa57b70415fad817a67ffc4e3
buitruyenpro <namtruyenpro95@gmail.com>
2023-09-30 17:31:53 +0700

    init project

---
+++ src/main/java/com/learningplatform/profilepostmanager/ProfilepostmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.learningplatform.profilepostmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilepostmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilepostmanagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/learningplatform/profilepostmanager/ProfilepostmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningplatform.profilepostmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilepostmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
