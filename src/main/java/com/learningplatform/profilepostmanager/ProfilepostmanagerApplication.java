3a731bac153d4ddd838d2e87cd29422a0e928cef
buitruyenpro <namtruyenpro95@gmail.com>
2023-09-30 22:26:53 +0700

    end lession-2

--- src/main/java/com/learningplatform/profilepostmanager/ProfilepostmanagerApplication.java
+++ src/main/java/com/learningplatform/profilepostmanager/ProfilepostmanagerApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfilepostmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfilepostmanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilepostmanagerApplication.class, args);
+    }
 }
+
+
